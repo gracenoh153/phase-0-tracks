b8ad111e6ac2afd8f602fe08fd0afd1304507851
gracenoh153 <gracenoh153@gmail.com>
2017-01-24 14:54:51 -0800

    create program that asks user input

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,13 +1,31 @@
 puts "What's the hamster's name?"
 hamster_name = gets.chomp
+
 puts "How noisy is the hamster from a scale of 1-10?"
-volume = gets.chomp
+volume = gets.chomp.to_i
+
+
 puts "What is the fur color?"
 fur_color = gets.chomp
+
+
 puts "Is the hamster a good candidate for adoption? y/n"
 adoption = gets.chomp
+
+
 puts "What is the hamster's estimated age?"
-age = gets.chomp
-  if age == " "
-    age = nil
-  end 
+input_age = gets.chomp
+
+if input_age.empty?
+  age = nil
+else
+  age = input_age.to_i
+end
+
+puts "Hamster's name: #{hamster_name}"
+puts "Volume level: #{volume}"
+puts "Fur color: #{fur_color}"
+puts "Good candidate for adoption: #{adoption}"
+puts "Age of hamster: #{age}"
+
+# paired with Brad Tamura
