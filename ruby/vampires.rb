7e1effd9478f83a41b454f417146588506dc8113
gracenoh153 <gracenoh153@gmail.com>
2017-01-24 20:22:18 -0800

    release 1: ask user input to check if they are a vampire

---
+++ ruby/vampires.rb
@@ -0,0 +1,13 @@
+puts "What's your name?"
+user_name = gets.chomp
+puts "How old are you?"
+age = gets.chomp.to_i
+puts "What year were you born?"
+birth_year = gets.chomp.to_i
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+garlic_bread = gets.chomp
+puts "Would you like to enroll in the company's health insurance?"
+insurance = gets.chomp
+
+
+
