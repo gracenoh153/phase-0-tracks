1f472dc6499a6d38a2113a4a56f36f06d2e2f265
gracenoh153 <gracenoh153@gmail.com>
2017-01-24 23:02:18 -0800

    detection logic

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,13 +1,28 @@
 puts "What's your name?"
 user_name = gets.chomp
 puts "How old are you?"
-age = gets.chomp.to_i
+user_age = gets.chomp.to_i
 puts "What year were you born?"
 birth_year = gets.chomp.to_i
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
 garlic_bread = gets.chomp
-puts "Would you like to enroll in the company's health insurance?"
+puts "Would you like to enroll in the company's health insurance? (y/n)"
 insurance = gets.chomp
 
+current_year = 2017
+age = current_year - birth_year
 
+if user_age == age && (garlic_bread == "y" || insurance == "y")
+  puts "Probably not a vampire."
+=begin
+elsif (age != (current_year - birth_year)) && ((garlic_bread == "n") || (insurance == "n"))
+  puts "Probably a vampire."
+elsif (age != (current_year - birth_year)) && ((garlic_bread == "n") && (insurance == "n"))
+  puts "Almost certainly a vampire"
+elsif (user_name == "Drake Cula") || (user_name == "Tu Fang")
+  puts "Definitely a vampire."
+else
+  puts "Results inconclusive."
+=end
 
+end 
