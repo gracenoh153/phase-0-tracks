76184b2de1a7317da4c2942c278e98121dcb53e5
gracenoh153 <gracenoh153@gmail.com>
2017-01-27 18:36:05 -0800

    add user input to encrypt or decrypt a password of their choice

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,57 +1,89 @@
-# Encrypt every letter by changing each character into the next one in the alphabet
+# create an encryption program that advances each letter of string forward one letter
+# define a method called "encrypt"
+# start index at 0, and while index is less than the string length,
+# for every letter "x" within the index, check if it is equal to "z"
+# if equal to "z", replace with the letter "a"
+# else, advance each letter by one letter
+# increase index until it is less than the string length
+# call method
+# print result of method 
 
-# Define a method to encrypt a string
-# .next every character in the string
-# print the new string
 
-# index the word and then apply .next to every indexed character
-
-# add conditional logic for edge case when string includes "z"
-# when encrypted string contains "z" convert output to "a"
-
-def encrypt(string1)
-  index = 0
-  while index < string1.length
-    alpha = "abcdefghijklmnopqrstuvwxyz"
-    if string1[index] == "z"
-      p "found it"
-      p encrypt_edge = (alpha.index(string1[index]))
-      p alpha[0]
-      p alpha[-1]
+def encrypt(str1)
+  index = 0 
+  while index < str1.length
+    for x in 0...str1.length
+      if str1[x] == "z"
+        str1[x] = "a"
+      else
+        str1[index] = str1[index].next 
+      end
+    index += 1  
     end 
-    string1[index] = string1[index].next
-    index += 1
-  end
+  end 
+p str1
+end 
 
-  puts string1
-end
-
-encrypt("abc")
-encrypt("zed")
+encrypt ("zed")
+encrypt ("zzd")
+encrypt ("abc")
 
 
-# Decryption
+# create a decryption program that reverses above encryption program
+# define a method called "decrypt"
+# put entire alphabet in a variable, in this case "alphabet"
+# start index at 0, and while index is less that the given string length,
+# for every letter "x" within the index, check if it is equal to "a"
+# if equal to "a", replace with "z"
+# else, go back one letter for each letter 
+# increase index until it is less than the given string length
+# call method
+# print result of method
 
-# Figure out where in the alphabet a letter is located
-# Subtract 1 from its index location
-# Convert index location to alphabet
-# Print result
-
-def decrypt(string2)
-  alpha = "abcdefghijklmnopqrstuvwxyz"
+def decrypt(str2)
   index = 0
-  while index < string2.length
-    decrypt_letter = alpha[(alpha.index(string2[index]) - 1)]
-    string2[index] = decrypt_letter
-    index += 1
-    if string2[index] == "a"
-      string2[index] = "z"
-    end  
-  end
-  puts string2
-end
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+  while index < str2.length
+    for x in 0...str2.length
+      if str2[x] == "a"
+        str2[x] = "z"
+      else
+        decrypt_letter = alphabet[(alphabet.index(str2[index]) - 1)]
+        str2[index] = decrypt_letter
+      end
+    index += 1  
+    end 
+  end 
+p str2
+end 
 
-decrypt("bcd")
-decrypt("afe")
+# decrypt ("bcd")
+# decrypt ("afe")
 
+# upgrade program to ask user if they want to encrypt or decrypt a password
+# ask user for password
+# print result then exit 
 
+# DRIVER CODE
+# ask user for preference
+puts "Do you want to decrypt or encrypt a password?"
+user_choice = gets.chomp
+  until user_choice == "encrypt" || user_choice == "decrypt"
+    puts "Not a valid choice. Choose encrypt or decrypt."
+    user_choice = gets.chomp
+  end 
+# ask user for password they want to encrypt/decrypt
+puts "What is the password?"
+password = gets.chomp
+# if choice is to encrypt, call "encrypt" method on user password
+  if user_choice == "encrypt"
+    encrypt(password)
+# if choice is to dectypt, call "decrypt" method on user password
+  else 
+    decrypt(password)
+  end 
+
+# nested method call test; will output "txpsegjti" then change back to "swordfish"
+# puts decrypt(encrypt("swordfish"))
+
+# paired with Nick Jarin 
