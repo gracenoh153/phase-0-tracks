e5a2a7acf2954baf3719c076703bb545c1abcbea
gracenoh153 <gracenoh153@gmail.com>
2017-01-28 11:25:20 -0800

    add extra code to loop until valid answer

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -3,24 +3,28 @@ hamster_name = gets.chomp
 
 puts "How noisy is the hamster from a scale of 1-10?"
 volume = gets.chomp.to_i
-
+  while (volume < 1) || (volume > 10)
+    puts "Invalid answer. Please enter a number between 1 and 10."
+    volume = gets.chomp.to_i
+  end 
 
 puts "What is the fur color?"
 fur_color = gets.chomp
 
-
 puts "Is the hamster a good candidate for adoption? y/n"
 adoption = gets.chomp
+  until (adoption.downcase == "y") || (adoption.downcase == "n")
+    puts "Invalid answer. Please enter \'y\' or \'n\'."
+    adoption = gets.chomp
+  end 
 
-
-puts "What is the hamster's estimated age?"
+puts "What is the hamster's estimated age? Leave blank if unknown."
 input_age = gets.chomp
-
-if input_age.empty?
-  age = nil
-else
-  age = input_age.to_i
-end
+  if input_age.empty?
+    age = nil
+  else
+    age = input_age.to_i
+  end
 
 puts "Hamster's name: #{hamster_name}"
 puts "Volume level: #{volume}"
