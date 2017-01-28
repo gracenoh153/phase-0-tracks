bb2904110149426ca3b3a6794f822852c51be0f8
gracenoh153 <gracenoh153@gmail.com>
2017-01-28 12:18:12 -0800

    add allergy loop

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,38 +1,66 @@
-# vampire detection... ask the user some basic questions
-puts "What's your name?"
-user_name = gets.chomp
+# process multiple employees
 
-puts "How old are you?"
-user_age = gets.chomp.to_i
+puts "How many employees do you want to process?"
+num_employees = gets.chomp.to_i
+employee_count = 0
+until employee_count == num_employees
 
-puts "What year were you born?"
-birth_year = gets.chomp.to_i
+  # vampire detection... ask the user some basic questions
 
-puts "Our company makes amazing garlic bread. Would you like some? (y/n)"
-garlic_bread = gets.chomp
-  until (garlic_bread.downcase == "y") || (garlic_bread.downcase == "n")
-    puts "Please enter \'y\' or \'n\'."
-    garlic_bread = gets.chomp
-  end
+  puts "What's your name?"
+  user_name = gets.chomp
 
-puts "Would you like to enroll in our company's health insurance? (y/n)"
-insurance = gets.chomp
-  until (insurance.downcase == "y") || (insurance.downcase == "n")
-    puts "Please enter \'y\' or \'n\'."
-    insurance = gets.chomp
-  end
+  puts "How old are you?"
+  user_age = gets.chomp.to_i
 
-# initiate vampire detection logic!
-current_year = 2017
-user_actual_age = current_year - birth_year
+  puts "What year were you born?"
+  birth_year = gets.chomp.to_i
 
-if (user_actual_age == user_age) && (garlic_bread == "y") || (insurance == "y")
-  puts "Probably not a vampire."
-elsif (user_actual_age != user_age) && (garlic_bread == "n") || (insurance == "n")
-  puts "Probably a vampire."
-elsif (user_actual_age != user_age) && (garlic_bread == "n") && (insurance == "n")
-  puts "Almost certainly a vampire."
+  puts "Our company makes amazing garlic bread. Would you like some? (y/n)"
+  garlic_bread = gets.chomp
+    until (garlic_bread.downcase == "y") || (garlic_bread.downcase == "n")
+      puts "Please enter \'y\' or \'n\'."
+      garlic_bread = gets.chomp
+    end
 
+  puts "Would you like to enroll in our company's health insurance? (y/n)"
+  insurance = gets.chomp
+    until (insurance.downcase == "y") || (insurance.downcase == "n")
+      puts "Please enter \'y\' or \'n\'."
+      insurance = gets.chomp
+    end
+
+  puts "Do you have any allergies? Enter them one at a time. When finished, type \'done\'."
+  allergies = gets.chomp
+    until allergies == "done"
+        if allergies.downcase == "sunshine"
+          break
+        else 
+          puts "Any other allergies?"
+          allergies = gets.chomp
+        end
+    end      
+
+  # initiate vampire detection logic!
+
+  current_year = 2017
+  user_actual_age = current_year - birth_year
+
+  if user_name == "Drake Cula" || user_name == "Tu Fang"
+    puts "Definitely a vampire."
+  elsif allergies.downcase == "sunshine"
+    puts "Probably a vampire."
+  elsif (user_actual_age != user_age) && (garlic_bread == "n" && insurance == "n")
+    puts "Almost certainly a vampire."
+  elsif (user_actual_age != user_age) && (garlic_bread == "n" || insurance == "n")
+    puts "Probably a vampire."
+  elsif (user_actual_age == user_age) && (garlic_bread == "y" || insurance == "y")
+    puts "Probably not a vampire."
+  else
+    puts "Results inconclusive."
+  end 
+
+  employee_count += 1
 end 
 
 =begin 
