269e1d954e3e85c4791603252d084144ebd5edec
gracenoh153 <gracenoh153@gmail.com>
2017-01-28 12:21:03 -0800

    delete commented out portion, add statement at the end that states that everyone can be friends

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -63,35 +63,4 @@ until employee_count == num_employees
   employee_count += 1
 end 
 
-=begin 
-
-puts "What's your name?"
-user_name = gets.chomp
-puts "How old are you?"
-user_age = gets.chomp.to_i
-puts "What year were you born?"
-birth_year = gets.chomp.to_i
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
-garlic_bread = gets.chomp
-puts "Would you like to enroll in the company's health insurance? (y/n)"
-insurance = gets.chomp
-
-current_year = 2017
-age = current_year - birth_year
-
-while true 
-  if (user_age == age) && ((garlic_bread == "y") || (insurance == "y"))
-    puts "Probably not a vampire."
-  elsif (age != (current_year - birth_year)) && ((garlic_bread == "n") || (insurance == "n"))
-    puts "Probably a vampire."
-  elsif (age != (current_year - birth_year)) && (garlic_bread == "n") && (insurance == "n")
-    puts "Almost certainly a vampire."
-  elsif (user_name == "Drake Cula") || (user_name == "Tu Fang")
-    puts "Definitely a vampire."
-  else
-    puts "Results inconclusive."
-  end 
-break
-end 
-
-=end 
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
