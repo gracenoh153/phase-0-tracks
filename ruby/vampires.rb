d6c6e7be219db8fca0486fa081223a2e64b97792
gracenoh153 <gracenoh153@gmail.com>
2017-01-28 11:55:48 -0800

    fix conditional statements

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,3 +1,42 @@
+# vampire detection... ask the user some basic questions
+puts "What's your name?"
+user_name = gets.chomp
+
+puts "How old are you?"
+user_age = gets.chomp.to_i
+
+puts "What year were you born?"
+birth_year = gets.chomp.to_i
+
+puts "Our company makes amazing garlic bread. Would you like some? (y/n)"
+garlic_bread = gets.chomp
+  until (garlic_bread.downcase == "y") || (garlic_bread.downcase == "n")
+    puts "Please enter \'y\' or \'n\'."
+    garlic_bread = gets.chomp
+  end
+
+puts "Would you like to enroll in our company's health insurance? (y/n)"
+insurance = gets.chomp
+  until (insurance.downcase == "y") || (insurance.downcase == "n")
+    puts "Please enter \'y\' or \'n\'."
+    insurance = gets.chomp
+  end
+
+# initiate vampire detection logic!
+current_year = 2017
+user_actual_age = current_year - birth_year
+
+if (user_actual_age == user_age) && (garlic_bread == "y") || (insurance == "y")
+  puts "Probably not a vampire."
+elsif (user_actual_age != user_age) && (garlic_bread == "n") || (insurance == "n")
+  puts "Probably a vampire."
+elsif (user_actual_age != user_age) && (garlic_bread == "n") && (insurance == "n")
+  puts "Almost certainly a vampire."
+
+end 
+
+=begin 
+
 puts "What's your name?"
 user_name = gets.chomp
 puts "How old are you?"
@@ -26,3 +65,5 @@ while true
   end 
 break
 end 
+
+=end 
