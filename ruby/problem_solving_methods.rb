f4cecd26cc5d908285257473133a902df5b4f2cc
gracenoh153 <gracenoh153@gmail.com>
2017-02-03 14:21:19 -0800

    define method for fibonacci sequence

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,3 +1,4 @@
+=begin 
 array = [15, 27, 31, 7, 80]
 def search_array(array, integer)
   index = 0
@@ -16,3 +17,22 @@ p search_array(array, 7)
 p search_array(array, 2)
 p search_array(array, 31)
 p search_array(array, 20)
+
+
+
+def fib(num)
+  fib_num = [0, 1]
+    while fib_num.length < num
+      fib_num << (fib_num[-2] + fib_num[-1])
+    end
+  return fib_num 
+end 
+
+p fib(6)
+p fib(100)
+=end 
+
+
+
+
+
