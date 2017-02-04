71db200c2959ee0a55a87945f4a42b58e3365ced
gracenoh153 <gracenoh153@gmail.com>
2017-02-03 17:43:01 -0800

    add Louvre and artwork

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,20 @@
+# places to visit in Paris, France 
+
+Paris_touristy_spots = {
+  musee_du_Louvre: {
+    paintings: [
+      "Mona Lisa", 
+      "The Lacemaker", 
+      "The Raft of the Medusa"
+      ],
+    sculptures: [
+      "Venus de Milo", 
+      "Diana of Versailles", 
+      "The Marly Horses"
+      ],
+    artists: [
+      "Leonardo da Vinci", 
+      "Johannes Vermeer", 
+      "Antonio Canova"
+      ],
+    } 
