f20b4fe85c1d1054292e3e2461f67778f0111b1a
gracenoh153 <gracenoh153@gmail.com>
2017-02-03 17:00:02 -0800

    make print out easier to read/understand for user

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -8,8 +8,10 @@
 - Print final version of hash, then exit program
 =end 
 
+# establish empty hash
 designer_client_info = {}
 
+# ask designer/user for client info then assign to empty hash
 puts "What is the client's name?"
 designer_client_info[:client_name] = gets.chomp
 
@@ -25,12 +27,21 @@ designer_client_info[:client_theme] = gets.chomp
 puts "How many rooms would the client like to decorate?"
 designer_client_info[:client_rooms] = gets.chomp.to_i
 
-puts "How many floors does their home have?"
+puts "How many floors does the client's home have?"
 designer_client_info[:client_floors] = gets.chomp.to_i
 
-p designer_client_info
+# print client info
+puts "*********************"
+puts "client_name: #{designer_client_info[:client_name]}"
+puts "client_age: #{designer_client_info[:client_age]}"
+puts "client_kids: #{designer_client_info[:client_kids]}"
+puts "client_theme: #{designer_client_info[:client_theme]}"
+puts "client_rooms: #{designer_client_info[:client_rooms]}"
+puts "client_floors: #{designer_client_info[:client_floors]}"
+puts "*********************"
 
-puts "Do any of the categories need to be changed/updated?"
+# give opportunity for hash to be corrected
+puts "Do any of the keys need to be changed/updated?"
 puts "If changes are needed, type 'yes'. If no changes are needed, type 'none'."
 make_changes = gets.chomp
 
@@ -42,4 +53,13 @@ if make_changes.downcase == "yes"
   designer_client_info[key_change.to_sym] = new_value
 end 
 
-p designer_client_info
+# print final version of hash 
+puts "This is your client's information."
+puts "*********************"
+puts "client_name: #{designer_client_info[:client_name]}"
+puts "client_age: #{designer_client_info[:client_age]}"
+puts "client_kids: #{designer_client_info[:client_kids]}"
+puts "client_theme: #{designer_client_info[:client_theme]}"
+puts "client_rooms: #{designer_client_info[:client_rooms]}"
+puts "client_floors: #{designer_client_info[:client_floors]}"
+puts "*********************"
