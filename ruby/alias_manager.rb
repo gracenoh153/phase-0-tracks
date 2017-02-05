936d8e7ac9300573fb99bf9596b6f9e3baaa34a4
gracenoh153 <gracenoh153@gmail.com>
2017-02-05 11:16:25 -0800

    start fresh

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -7,30 +7,3 @@
 # Create empty hash that will store user names
 # Print results to screen
 
-def convert_letters(name)
-  vowels = "aeiou"
-  if vowels.include?(name)
-    if name == "u"
-      name = "a"
-    else
-      name = vowels[vowels.index(name) + 1]
-    end
-  end
-  consonants = "bcdfghjklmnpqrstvwxyz"
-  if name == "z"
-    name = "b"
-  else
-    name = consonants[consonants.index(name) + 1]
-  end
-end
-
-def alias(name)
-  new_alias = ""
-  swapped_name = name.split(" ")
-  swapped_name.reverse.join(" ")
-  swapped_name.
-end 
-
-
-
-
