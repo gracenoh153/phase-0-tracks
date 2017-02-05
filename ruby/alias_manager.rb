2c7486861a6c1b06d92382c44ed2785bc3b42efd
gracenoh153 <gracenoh153@gmail.com>
2017-02-05 11:32:14 -0800

    add little pieces of program... covert vowels'

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -7,3 +7,29 @@
 # Create empty hash that will store user names
 # Print results to screen
 
+=begin
+def swap_first_last(name)
+  swapped_name = real_name.split(' ').reverse.join(' ')
+end 
+=end
+
+def convert_vowel(letter)
+  vowels = "aeiou"
+  if vowels.include?(letter)
+    if letter == "u"
+      letter = "a"
+    else
+      letter = vowels[vowels.index(letter) + 1]
+    end
+  end
+end 
+
+
+
+
+=begin
+puts "I am the alias generator."
+puts "Please enter your first and last name."
+real_name = gets.chomp
+
+=end 
