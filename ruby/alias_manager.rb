126abde8ddfc613bcb00b3f89918d5b1ead90318
gracenoh153 <gracenoh153@gmail.com>
2017-02-05 12:04:38 -0800

    add new method for rotating vowels

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -7,41 +7,20 @@
 # Create empty hash that will store user names
 # Print results to screen
 
-=begin
-def swap_first_last(name)
-  swapped_name = real_name.split(' ').reverse.join(' ')
-end 
-=end
 
-def convert_vowel(letters)
-  vowels = "aeiou"
-  if vowels.include?(letters)
-    if letter == "u"
-      letter = "a"
+def convert_vowels(user_name)
+  vowels = ['a', 'e', 'i', 'o', 'u']
+  user_name = user_name.split('')
+  new_letters = user_name.map do |letter|
+    if vowels.include?(letter)
+      vowels.rotate(1)[vowels.index(letter)]
     else
-      letter = vowels[vowels.index(letters) + 1]
+      letter
     end
   end
+  new_letters.join
 end 
 
-def convert_consonants(letters)
-  consonants = "bcdfghjklmnpqrstvwxyz"
-  if consonants.include?(letters)
-    if letters == "z"
-      letters = "a"
-    else
-      letters = consonants[consonants.index(letters) + 1]
-    end
-  end
-end
+# p convert_vowels("Gracu")
 
 
-
-
-
-=begin
-puts "I am the alias generator."
-puts "Please enter your first and last name."
-real_name = gets.chomp
-
-=end 
