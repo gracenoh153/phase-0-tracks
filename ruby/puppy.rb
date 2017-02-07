ba4eed05cda7702a4ae825dd495a6beefcc851bd
gracenoh153 <gracenoh153@gmail.com>
2017-02-07 08:36:24 -0800

    add puppy class; design it using comments

---
+++ ruby/puppy.rb
@@ -0,0 +1,21 @@
+# PUPPY class (aka the species) --------
+# dog
+
+# CHARACTERISTICS (aka the attributes) --------
+# name: varies
+# color: varies
+# size: varies
+# eye count: 2
+# leg count: 4
+# sheds: varies
+# noise: bark
+
+# BEHAVIOR (aka the methods) --------
+# bark
+# whine
+# howl
+# pant
+# lick
+# growl
+# jump
+# fetch
