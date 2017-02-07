aadfb904b8471c7c7b796d8e6d23c494a4a4480a
gracenoh153 <gracenoh153@gmail.com>
2017-02-07 12:02:32 -0800

    add methods to puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,21 @@ class Puppy
     toy
   end
 
-end
+  def speak(int)
+    puts 'woof!' * int
+  end
 
+  def rollover
+    puts "*rollover*"
+  end
+
+  def dog_years(human_year)
+    dog_year = human_year * 7
+    puts dog_year
+  end
+
+  def jump
+    puts "*jumps*"
+  end
+
+end
