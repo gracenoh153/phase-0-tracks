7747f561cb4bae54fa849c2f9cfa39c53fd16b25
gracenoh153 <gracenoh153@gmail.com>
2017-02-09 23:10:24 -0800

    change order

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,7 @@
 class Santa
 
-  def speak
-    puts "Ho, ho, ho! Haaaappy holidays!"
+  def initialize
+    puts "Initilizing Santa instance ..."
 
     @gender = [
       "agender", "androgynous", "bigender", 
@@ -20,14 +20,16 @@ class Santa
       "Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", 
       "Comet", "Cupid", "Donner", "Blitzen"
     ]
+
+    @age = 0
+  end 
+
+  def speak
+    puts "Ho, ho, ho! Haaaappy holidays!"
   end 
   
   def eat_milk_and_cookies(cookie_type)
     puts "That was a good #{cookie_type}!"
-  end 
-  
-  def initialize
-    puts "Initilizing Santa instance ..."
   end 
 
 end 
