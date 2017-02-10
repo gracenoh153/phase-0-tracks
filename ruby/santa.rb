806a63c60d600e80b96ea2174e5fb2e8e437018e
gracenoh153 <gracenoh153@gmail.com>
2017-02-10 10:45:10 -0800

    edit code for release 1

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,27 +1,29 @@
 class Santa
 
-  def initialize
+  def initialize(gender, ethnicity)
     puts "Initilizing Santa instance ..."
 
-    @gender = [
-      "agender", "androgynous", "bigender", 
-      "cisgender", "gender fluid", "female",
-      "male", "non-binary", "pangender", "queer",
-      "transgender female", "transgender male", "other"
-    ]
+      @gender = gender 
+      # [
+      #  "agender", "androgynous", "bigender", 
+      #  "cisgender", "gender fluid", "female",
+      #  "male", "non-binary", "pangender", "queer",
+      #  "transgender female", "transgender male", "other"
+      #]
 
-    @ethnicity = [
-      "White", "Black or African American", "American Indian",
-      "Alaska Native", "Asian", "Pacific Islander", "Native Hawaiian",
-      "Hispanic, Latino, or Spanish Origin", "other"
-    ]
+      @ethnicity = ethnicity 
+      #[
+      #  "White", "Black or African American", "American Indian",
+      #  "Alaska Native", "Asian", "Pacific Islander", "Native Hawaiian",
+      #  "Hispanic, Latino, or Spanish Origin", "other"
+      # ]
 
-    @reindeer_ranking = [
-      "Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", 
-      "Comet", "Cupid", "Donner", "Blitzen"
-    ]
+      @reindeer_ranking = [
+        "Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", 
+        "Comet", "Cupid", "Donner", "Blitzen"
+      ]
 
-    @age = 0
+      @age = 0
   end 
 
   def speak
@@ -37,16 +39,42 @@ class Santa
   end 
 
   def get_mad_at(reindeer_name)
-    p @reindeer_ranking
     @reindeer_ranking.delete(reindeer_name)
-    @reindeer_ranking.push(reindeer_name)
+    @reindeer_ranking << reindeer_name
     puts "Behave, #{reindeer_name}! Go to the back of the line."
     p @reindeer_ranking
   end 
 end 
 
-# santa = Santa.new 
-# santa.speak
-# santa.eat_milk_and_cookies("sugar cookie")
+santa = Santa.new("male", "Santa")
+santa.speak
+santa.eat_milk_and_cookies("sugar cookie")
 
+=begin 
 santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
+=end 
+
+=begin 
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+=end 
+=begin 
+santas = []
+example_names = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid"]
+example_ethnicities = ["White", "Black or African American", "American Indian", 
+  "Alaska Native", "Asian", "Pacific Islander", "Native Hawaiian"]
+example_names.length.times do |index|
+  santas << Santa.new(example_names[index], example_ethnicities[index])
+end 
+=end 
