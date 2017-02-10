00d6d8c370bde89c9615dbdb274e82ff2665df2a
gracenoh153 <gracenoh153@gmail.com>
2017-02-09 23:01:43 -0800

    add gender attribute

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -2,6 +2,18 @@ class Santa
 
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
+
+    @gender = [
+      "agender", "androgynous", "bigender", 
+      "cisgender", "gender fluid", "female",
+      "male", "non-binary", "pangender", "queer",
+      "transgender female", "transgender male", "other"
+    ]
+
+    @ethnicity = [
+      
+
+    ]
   end 
   
   def eat_milk_and_cookies(cookie_type)
@@ -13,3 +25,8 @@ class Santa
   end 
 
 end 
+
+# santa = Santa.new 
+# santa.speak
+# santa.eat_milk_and_cookies("sugar cookie")
+
