fb0fa483f96b9e1bf3c00493376371db1c51cc3d
gracenoh153 <gracenoh153@gmail.com>
2017-02-10 13:00:59 -0800

    create a shout module with two methods; yell angrily and yell happily

---
+++ ruby/shout.rb
@@ -0,0 +1,15 @@
+=begin 
+module Shout
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yelling_happily(words)
+    words + "!!!" + " :D"
+  end 
+end
+
+puts Shout.yell_angrily("I can't believe they're out of chicken pot pie again")
+puts Shout.yelling_happily("They're serving my favorite dessert today")
+=end 
+
