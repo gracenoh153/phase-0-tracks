02d2ac778dcfba690a725ce5d980df3d24f4c7bf
gracenoh153 <gracenoh153@gmail.com>
2017-02-12 21:49:25 -0800

    add initial commit for rpsec file

---
+++ ruby/game/guessword_spec.rb
@@ -0,0 +1,17 @@
+require_relative 'GuessWord'
+
+describe GuessWord do 
+  let (:guessword) {GuessWord.new}
+  it "" do
+    expect
+  end
+  it "" do
+    expect
+  end 
+  it "" do
+    expect
+  end 
+  it "" do
+    expect 
+  end
+end
