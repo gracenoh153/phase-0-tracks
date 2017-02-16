42e026089254e9609a61d733118060dec49ba6c6
gracenoh153 <gracenoh153@gmail.com>
2017-02-15 20:45:32 -0800

    add pseudocode logic

---
+++ js/explore.js
@@ -0,0 +1,9 @@
+// create a function that will take a string parameter, then reverse it
+  // set new variable equal to empty string
+  // write a for loop with following statements:
+    // sets "i" equal to (string length - 1); this will be the last character of the string
+    // set "i" greater than or equal to 0, so that the loop ends once it's done iterating thru string
+    // subtract 1 from "i" each time through the loop 
+
+  // example: reverse("hello") should return "olleh"
+
