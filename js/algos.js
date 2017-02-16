dda5a5fcfafe83e61448191f35450b6f30c87e31
gracenoh153 <gracenoh153@gmail.com>
2017-02-15 22:19:58 -0800

    add for and if loop for comparing strings within arrays

--- js/algos.js
+++ js/algos.js
@@ -1,14 +1,18 @@
 // find the longest phrase
   // create function that takes an array as a parameter
   // create a loop inside function that iterates through each element of the array
-    // get length each element of the array, then compare the lengths
+    // get length of each element of the array
+    // if current element length is longer than the one before it, store as longest string
   // return longest element of the array as a string
 
 function findLongestString(string) {
+  
   var longestString = "";
 
   for (var i = 0; i < string.length; i++) {
-    
+      if (string[i].length > longestString.length) {
+        longestString = string[i];
+      }
   }
-
+  return longestString; 
 }
