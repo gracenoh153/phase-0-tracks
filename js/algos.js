41f1e9f3da9c54fb876435d49577f594b90c73ec
gracenoh153 <gracenoh153@gmail.com>
2017-02-15 22:21:50 -0800

    add driver code to test longest string

--- js/algos.js
+++ js/algos.js
@@ -4,6 +4,7 @@
     // get length of each element of the array
     // if current element length is longer than the one before it, store as longest string
   // return longest element of the array as a string
+  // 
 
 function findLongestString(string) {
   
@@ -16,3 +17,11 @@ function findLongestString(string) {
   }
   return longestString; 
 }
+
+// DRIVER CODE
+
+console.log(findLongestString(["long phrase","longest phrase","longer phrase"]))
+console.log(findLongestString(["hello", "mush", "celebration"]))
+
+
+// create function that takes two objects; check if they share at least one key-value pair
