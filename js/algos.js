6d3f2d730d2ca1912267dd8305f2d8991b0e54d0
gracenoh153 <gracenoh153@gmail.com>
2017-02-15 21:22:54 -0800

    initial commit with pseudocode

---
+++ js/algos.js
@@ -0,0 +1,12 @@
+// find the longest phrase
+  // create function that takes an array as a parameter
+  // create a loop inside function that iterates through each element of the array
+    // get length each element of the array, then compare the lengths
+  // return longest element of the array as a string
+
+function findLongestString(string) {
+  var longestString = "";
+
+  
+
+}
