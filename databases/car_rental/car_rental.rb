8efe0dc3e7c6d11d6a7717c794ffaba28c3c114d
gracenoh153 <gracenoh153@gmail.com>
2017-02-25 22:22:16 -0800

    add pseudocode

---
+++ databases/car_rental/car_rental.rb
@@ -0,0 +1,11 @@
+# create a car rental program
+# allows car rental company to assign rental cars to customers 
+  # ask customer car size/mileage preference
+  # ask customer car brand preference
+# create database that holds following info:
+  # table for customer id and name
+  # table for car id and brand name
+  # table for car specs
+# match customer preferences to car available
+# ... add to this later on as I build the program
+
