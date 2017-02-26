32f214c9174740ea1f0ba83e93d4ea385f8c83a6
gracenoh153 <gracenoh153@gmail.com>
2017-02-25 22:36:17 -0800

    require gem, creat user table

--- databases/car_rental/car_rental.rb
+++ databases/car_rental/car_rental.rb
@@ -9,3 +9,17 @@
 # match customer preferences to car available
 # ... add to this later on as I build the program
 
+# require gem
+require 'sqlite3'
+
+# create SQLite3 database
+db = SQLite3::Database.new("rental.db")
+
+# create table for customers
+create_customers_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS customers(
+    id INTEGER PRIMARY KEY
+    first_name VARCHAR(255)
+    last_name VARCHAR(255)
+  )
+SQL
