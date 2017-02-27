81be4fe8e4c7c94ccd95f2d9faf4d53b55e930cf
gracenoh153 <gracenoh153@gmail.com>
2017-02-27 00:14:27 -0800

    make program simpler

--- databases/grocery_list/groceries.rb
+++ databases/grocery_list/groceries.rb
@@ -16,7 +16,7 @@ create_users_table = <<-SQL
 SQL
 
 grocery_list.execute(create_users_table)
-=end 
+
 
 # create stores table 
 create_stores_table = <<-SQL
@@ -27,6 +27,7 @@ create_stores_table = <<-SQL
 SQL
 
 grocery_list.execute(create_stores_table)
+=end 
 
 # create items table 
 create_items_table = <<-SQL
@@ -35,34 +36,34 @@ create_items_table = <<-SQL
     item VARCHAR(255),
     type VARCHAR(255),
     quantity INT,
-    perishable BOOLEAN,
-    user_id INT,
-    store_id INT,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (store_id) REFERENCES stores(id)
+    perishable BOOLEAN
   )
 SQL
+    # user_id INT,
+    # store_id INT,
+    # FOREIGN KEY (user_id) REFERENCES users(id),
+    # FOREIGN KEY (store_id) REFERENCES stores(id)
 
 grocery_list.execute(create_items_table)
 
 # manipulate stores table
-def add_stores(grocery_list, name)
-  grocery_list.execute("INSERT INTO stores (name) VALUES (?)", [name])
-end 
+# def add_stores(grocery_list, name)
+#   grocery_list.execute("INSERT INTO stores (name) VALUES (?)", [name])
+# end 
 
-def delete_stores(grocery_list, id_number)
-  grocery_list.execute("DELETE FROM stores WHERE id = #{id_number}")
-end 
+# def delete_stores(grocery_list, id_number)
+#   grocery_list.execute("DELETE FROM stores WHERE id = #{id_number}")
+# end 
 
-def print_stores(grocery_list, store)
-  store_list = grocery_list.execute("SELECT * FROM stores")
-  store_list.each do |store|
-    puts "#{store[id]} - #{stores['name']}"
-  end 
-end 
+# def print_stores(grocery_list, store)
+#   store_list = grocery_list.execute("SELECT * FROM stores")
+#   store_list.each do |store|
+#     puts "#{store[id]} - #{stores['name']}"
+#   end 
+# end 
 
 # manipulate items table
-def add_items(grocery_list, item, type, quantity, persishable)
+def add_items(grocery_list, item, type, quantity, perishable)
   grocery_list.execute("INSERT INTO items (item, type, quantity, perishable) VALUES (?, ?, ?)", [item, type, quantity, "false"])
 end 
 
@@ -84,15 +85,14 @@ puts "Welcome to the grocery list maker!"
 puts "Choose one of the following options:"
 loop do 
   puts "1 - Create a new grocery list"
-  puts "2 - Modify list of stores"
-  puts "3 - Modify list of items"
-  puts "4 - View your list"
+  puts "2 - View your list"
+  puts "3 - Modify your list"
   puts "'q' - Quit"
   user_choice = gets.chomp 
 
   break if user_choice == "q"
 
-  if user_choice == "1"
+  if user_choice.to_i == 1
     puts "Creating a new grocery list."
     puts "What is the item you would like to purchase?"
     item = gets.chomp
@@ -100,18 +100,45 @@ loop do
     type = gets.chomp
     puts "Enter the quantity you would like to buy."
     quantity = gets.chomp
-    puts "Is the item perishable? Enter 'yes' or 'no'."
-    is_persishable = gets.chomp
-      if is_persishable == 'yes'
-        persishable = true
-      else 
-        persishable = false
-      end 
+    puts "Is the item perishable? Enter 'true' or 'false'."
+    perishable = gets.chomp
+    add_items(grocery_list, item, type, quantity, perishable)
     puts "Here is the information you just entered:"
     print_items(grocery_list)
 
-  elsif user_choice == "2" 
+  elsif user_choice.to_i == 2
+    puts "If you would like to add a store, enter 'add'."
+    puts "If you would like to delete a store, enter 'delete'."
+    store_choice = gets.chomp
+      if store_choice == 'add'
+        puts "Enter the name of the store you would like to add."
+        store_name = gets.chomp 
+        add_stores(grocery_list, store_name)
+      elsif store_choice == 'delete'
+        puts "Enter the ID number of the store you would like to delete."
+        id = gets.chomp
+        delete_stores(grocery_list, id)
+      end 
+    puts "Your stores list includes the following:"
+    print_stores(grocery_list, store)
+
+  elsif user_choice.to_i == 3 
+    puts "Here is your list of items to buy:"
+    print_items(grocery_list, items)
+    puts "If you would like to add an item, enter 'add'."
+    puts "If you would like to delete an item, enter 'delete'."
+    item_choice = gets.chomp 
+      if item_choice == 'add'
+        puts "Enter the name of the store you would like to add."
+        store_name = gets.chomp 
+        add_items(grocery_list, store_name)
+      elsif item_choice == 'delete'
+        puts "Enter the ID number of the store you would like to delete."
+        id = gets.chomp
+        delete_items(grocery_list, id)
+      end 
   end 
+
 
 
 end 
