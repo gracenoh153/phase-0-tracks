59b62d2d3c21937a28787dc70c962db933fd9a3e
gracenoh153 <gracenoh153@gmail.com>
2017-02-27 00:36:51 -0800

    make code simpler

--- databases/grocery_list/groceries.rb
+++ databases/grocery_list/groceries.rb
@@ -3,7 +3,7 @@ require 'sqlite3'
 
 # create database for grocery list 
 grocery_list = SQLite3::Database.new("groceries.db")
-grocery_list.results_as_hash = true
+# grocery_list.results_as_hash = true
 
 =begin 
 # create users table 
@@ -64,7 +64,7 @@ grocery_list.execute(create_items_table)
 
 # manipulate items table
 def add_items(grocery_list, item, type, quantity, perishable)
-  grocery_list.execute("INSERT INTO items (item, type, quantity, perishable) VALUES (?, ?, ?)", [item, type, quantity, "false"])
+  grocery_list.execute("INSERT INTO items (item, type, quantity, perishable) VALUES (?, ?, ?, ?)", [item, type, quantity, "false"])
 end 
 
 def delete_items(grocery_list, id_number)
@@ -76,36 +76,42 @@ end
 def print_items(grocery_list)
   item_list = grocery_list.execute("SELECT * FROM items")
   item_list.each do |item|
-    puts "#{item[id]} - item: #{items['item']}, type: #{items['type']}, quantity: #{items['quantity']}, perishable: #{items['perishable']}" 
+    puts "#{items['id']} - item: #{items['item']}, type: #{items['type']}, quantity: #{items['quantity']}, perishable: #{items['perishable']}" 
   end 
 end
 
 # USER INTERFACE
 puts "Welcome to the grocery list maker!"
 puts "Choose one of the following options:"
-loop do 
-  puts "1 - Create a new grocery list"
-  puts "2 - View your list"
-  puts "3 - Modify your list"
-  puts "'q' - Quit"
-  user_choice = gets.chomp 
+  loop do 
+    puts "1 - Create a new grocery list"
+    puts "2 - View your list"
+    puts "'q' - Quit"
+    user_choice = gets.chomp 
 
-  break if user_choice == "q"
+    break if user_choice == "q"
 
-  if user_choice.to_i == 1
-    puts "Creating a new grocery list."
-    puts "What is the item you would like to purchase?"
-    item = gets.chomp
-    puts "What type of item is it? (e.g. food, toiletries, hardware, gardening, etc...."
-    type = gets.chomp
-    puts "Enter the quantity you would like to buy."
-    quantity = gets.chomp
-    puts "Is the item perishable? Enter 'true' or 'false'."
-    perishable = gets.chomp
-    add_items(grocery_list, item, type, quantity, perishable)
-    puts "Here is the information you just entered:"
-    print_items(grocery_list)
+    if user_choice.to_i == 1
+      puts "Creating a new grocery list."
+      puts "What is the item you would like to purchase?"
+      item = gets.chomp
+      puts "What type of item is it? (e.g. food, toiletries, hardware, gardening, etc...."
+      type = gets.chomp
+      puts "Enter the quantity you would like to buy."
+      quantity = gets.chomp.to_i
+      puts "Is the item perishable? Enter 'true' or 'false'."
+      perishable = gets.chomp
+      add_items(grocery_list, item, type, quantity, perishable)
+      puts "Here is the information you just entered:"
+      print_items(grocery_list)
+    elsif user_choice.to_i == 2 
+      puts "Here is your grocery list!"
+      puts "-" * 20 
+      print_items(grocery_list)
+    end 
+  end 
 
+=begin 
   elsif user_choice.to_i == 2
     puts "If you would like to add a store, enter 'add'."
     puts "If you would like to delete a store, enter 'delete'."
@@ -116,7 +122,7 @@ loop do
         add_stores(grocery_list, store_name)
       elsif store_choice == 'delete'
         puts "Enter the ID number of the store you would like to delete."
-        id = gets.chomp
+        id = gets.chomp.to_i
         delete_stores(grocery_list, id)
       end 
     puts "Your stores list includes the following:"
@@ -134,13 +140,17 @@ loop do
         add_items(grocery_list, store_name)
       elsif item_choice == 'delete'
         puts "Enter the ID number of the store you would like to delete."
-        id = gets.chomp
+        id = gets.chomp.to_i 
         delete_items(grocery_list, id)
       end 
   end 
+=end 
 
 
 
-end 
+
+
+# definitely want to come back and make this into a more useful program... 
+# adding stores, modifying information..
 
 
