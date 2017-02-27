f6cbd334d0540cb7e9b70532a38f0c1b5032f936
gracenoh153 <gracenoh153@gmail.com>
2017-02-26 16:40:49 -0800

    add items table

--- databases/grocery_list/groceries.rb
+++ databases/grocery_list/groceries.rb
@@ -3,15 +3,15 @@ require 'sqlite3'
 grocery_list = SQLite3::Database.new("groceries.db")
 grocery_list.results_as_hash = true
 
-create_user_table = <<-SQL
-  CREATE TABLE IF NOT EXISTS user(
+create_users_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS users(
     id INTEGER PRIMARY KEY,
     first_name VARCHAR(255),
     last_name VARCHAR(255)
   )
 SQL
 
-grocery_list.execute(create_user_table)
+grocery_list.execute(create_users_table)
 
 create_stores_table = <<-SQL
   CREATE TABLE IF NOT EXISTS stores(
@@ -21,4 +21,17 @@ create_stores_table = <<-SQL
 SQL
 
 grocery_list.execute(create_stores_table)
+
+create_items_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS items(
+    id INTEGER PRIMARY KEY,
+    item VARCHAR(255),
+    quantity INT,
+    perishable BOOLEAN,
+    user_id INT,
+    store_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (store_id) REFERENCES stores(id)
+  )
+SQL
 
