52bba9fd8c2a1408bb5417df103b3cebd604ef91
gracenoh153 <gracenoh153@gmail.com>
2017-02-26 17:44:42 -0800

    create method to add users

--- databases/grocery_list/groceries.rb
+++ databases/grocery_list/groceries.rb
@@ -1,8 +1,11 @@
+# require gem 
 require 'sqlite3'
 
+# create database for grocery list 
 grocery_list = SQLite3::Database.new("groceries.db")
 grocery_list.results_as_hash = true
 
+# create users table 
 create_users_table = <<-SQL
   CREATE TABLE IF NOT EXISTS users(
     id INTEGER PRIMARY KEY,
@@ -13,6 +16,7 @@ SQL
 
 grocery_list.execute(create_users_table)
 
+# create stores table 
 create_stores_table = <<-SQL
   CREATE TABLE IF NOT EXISTS stores(
     id INTEGER PRIMARY KEY,
@@ -22,6 +26,7 @@ SQL
 
 grocery_list.execute(create_stores_table)
 
+# create items table 
 create_items_table = <<-SQL
   CREATE TABLE IF NOT EXISTS items(
     id INTEGER PRIMARY KEY,
@@ -36,4 +41,9 @@ create_items_table = <<-SQL
 SQL
 
 grocery_list.execute(create_items_table)
+
+# allow users to be added 
+def add_users(first_name, last_name)
+  grocery_list.execute("INSERT INTO users (first_name, last_name) VALUES (?,?)", [first_name, last_name]) 
+end
 
