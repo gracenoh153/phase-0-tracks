d0cd13cd933d77026d96cfe176bbf21e2e8d7d44
gracenoh153 <gracenoh153@gmail.com>
2017-03-03 21:11:06 -0800

    add jQuery

---
+++ web_dev/jquery/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  console.log("This script is now running!");
+
+  $("p").hover(function() {
+    $("p").css("background-color", "pink");
+    $("p").css("border", "3px dashed #f442bc");
+  });
+
+  $("#Badook").click(function() {
+    $("#Badook").css("border", "10px dotted blue");
+  });
+
+  $("")
+
+
+
+
+
+});
